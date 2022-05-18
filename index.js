b13fb590d9236f79bb3ad78e8315d37a66472efc
Cody B. Daig <cody@daig.me>
2022-05-18 15:18:36 -0400

    type app_name

--- index.js
+++ index.js
@@ -18,30 +18,11 @@ let heroku = {
   api_key: core.getInput("heroku_api_key"),
   email: core.getInput("heroku_email"),
   app_name: core.getInput("heroku_app_name"),
-  buildpack: core.getInput("buildpack"),
-  branch: core.getInput("branch"),
-  dontuseforce: core.getInput("dontuseforce") === "false" ? false : true,
-  dontautocreate: core.getInput("dontautocreate") === "false" ? false : true,
-  usedocker: core.getInput("usedocker") === "false" ? false : true,
-  dockerHerokuProcessType: core.getInput("docker_heroku_process_type"),
-  dockerBuildArgs: core.getInput("docker_build_args"),
-  appdir: core.getInput("appdir"),
-  healthcheck: core.getInput("healthcheck"),
-  checkstring: core.getInput("checkstring"),
-  delay: parseInt(core.getInput("delay")),
-  procfile: core.getInput("procfile"),
-  rollbackonhealthcheckfailed:
-    core.getInput("rollbackonhealthcheckfailed") === "false" ? false : true,
-  env_file: core.getInput("env_file"),
-  justlogin: core.getInput("justlogin") === "false" ? false : true,
-  region: core.getInput("region"),
-  stack: core.getInput("stack"),
-  team: core.getInput("team"),
 };
 
 try {
   execSync(createCatFile({ email: heroku.email, api_key: heroku.api_key }));
-  execSync("heroku git:remote --app " + app_name);
+  execSync("heroku git:remote --app " + heroku.app_name);
   execSync(`git push heroku HEAD:master --force`);
   core.setOutput("status", "Successfully deployed staging");
 } catch (err) {
